f3272bc4070d9056bd3caff76e6e66cd142d409c
TheSh4h <hh.haseebshah@gmail.com>
2023-10-02 07:54:11 -0700

    configured store and slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import Counter from "./Counter"
 import "./App.css"
+import Navbar from "./components/Navbar"
+import UserDetails from "./components/UserDetails"
 
 function App() {
   
   return (
     <>
-      <Counter />
+      <Navbar />
+      <UserDetails />
     </>
   )
 }
--- src/reducers/index.jsx
+++
@@ -1,8 +0,0 @@
-import counterReducer from './counterReducer';
-import { combineReducers } from 'redux';
-
-const rootReducer = combineReducers({
-    counterReducer
-});
-
-export default rootReducer;
