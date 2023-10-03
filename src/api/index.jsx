1dd5e4aa1deb8b415facae97108f57ef6cc76f59
TheSh4h <hh.haseebshah@gmail.com>
2023-10-03 10:15:13 -0700

    cleansed

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -3,8 +3,6 @@ import Chance from "chance";
 const chance = Chance();
 
 export const fakeUserData = () => {
-    console.log(chance.name({ middle: true }));
+    //console.log(chance.name({ middle: true }));
     return chance.name({ middle: true });
 }
-
-fakeUserData();
