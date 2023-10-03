f5aba800f410598021c592d5871afc6bd1f35dd1
TheSh4h <hh.haseebshah@gmail.com>
2023-10-03 10:03:24 -0700

    add User actionCreator added

--- src/store/slices/UserSlice.jsx
+++ src/store/slices/UserSlice.jsx
@@ -4,12 +4,16 @@ const userSlice = createSlice({
     name: "user",
     initialState: [],
     reducers: {
-        addUser(state, action) {},
+        addUser(state, action) {
+            state.push(action.payload);
+            console.log(action.payload)
+        },
         removeUser(state, action) {},
         deleteAllUsers(state, action) {}
     },
 })
 
-console.log(userSlice)
+console.log(userSlice.actions)
 
 export default userSlice.reducer ;
+export const  { addUser } = userSlice.actions;
